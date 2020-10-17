2a2aac9bf79add6ffe2b2d0b31f27887c7881dea
NamelessKingMeow <ubolchai2000@gmail.com>
2020-10-17 11:10:02 +0700

    finaly user_update is work
     finaly user_update is work

--- user_update_SQL.php
+++ user_update_SQL.php
@@ -8,14 +8,14 @@ $user_address = $_REQUEST['user_address'];
 $user_email  = $_REQUEST['user_email'];
 $user_tel  = $_REQUEST['user_tel'];
 
-$inster_item = $conn->prepare("UPDATE `user` SET 
-`user_fullname`=`$user_fullname`,
-`user_address`=`$user_address`,
-`user_email`=`$user_email`,
-`user_tel`=`$user_tel`
-WHERE `user_username` = '$user_username';");
-$inster_item->execute();
+$inster_user = $conn->prepare("UPDATE user SET 
+user_fullname='$user_fullname',
+user_address='$user_address',
+user_email='$user_email',
+user_tel='$user_tel'
+WHERE user_username = '$user_username';");
+$inster_user->execute();
 
 
 
-header("Location: admin.php");
+header("Location: user_data.php");
