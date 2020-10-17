acf0ba89e9a8d5c33c4410dbb3de656dcffac716
NamelessKingMeow <ubolchai2000@gmail.com>
2020-10-17 23:01:23 +0700

    payment is work !!!

--- payment_detail.php
+++ payment_detail.php
@@ -32,7 +32,7 @@ $order_id = $_REQUEST['order_id'];
 
     <div class="container mt-3">
         <h1>แจ้งชำระเงิน </h1>
-        <h2>หมายเลขธุระกรรม <?php echo $pay_id; ?> ของ <a href="order_detail.php?order_id=<?php echo $order_id; ?>">ORDER #<?php echo $order_id; ?></a></h2>
+        <h2>หมายเลขธุระกรรม #<?php echo $pay_id; ?> ของ <a href="order_detail.php?order_id=<?php echo $order_id; ?>">ORDER #<?php echo $order_id; ?></a></h2>
         <div class="col-12">
             <div class=" card table-responsive">
                 <h3>สิ่งของใน order</h3>
@@ -46,39 +46,63 @@ $order_id = $_REQUEST['order_id'];
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <?php
-                            $count = 1;
-                            $total = 0;
-                            $call_order_item = $conn->prepare("SELECT * FROM orders_no where order_no_id ='$order_id' ");
-                            $call_order_item->execute();
-                            while ($result = $call_order_item->fetch()) { ?> 
+
+                        <?php
+                        $count = 1;
+                        $total = 0;
+                        $call_order_item = $conn->prepare("SELECT * FROM orders_no where order_no_id ='$order_id' ");
+                        $call_order_item->execute();
+
+                        while ($result = $call_order_item->fetch()) { ?>
+                            <tr>
                                 <?php
-                                    $item_id = $result["order_no_item"];
-                                    $call_item = $conn->prepare("SELECT * FROM item where id_item ='$item_id' ");
-                                    $call_item->execute();
-                                    $result_item =$call_item->fetch();
+                                $item_id = $result["order_no_item"];
+                                $call_item = $conn->prepare("SELECT * FROM item where id_item ='$item_id' ");
+                                $call_item->execute();
+                                $result_item = $call_item->fetch();
                                 ?>
 
                                 <th scope="row"><?php echo $count; ?></th>
-                                <td><a href="item_detail.php?id_item=<?php echo $result_item['id_item']; ?>"><img  src="image/item/<?php echo $result_item["imagelocation"] ?>" alt="..." class="   " height="50px"> <?php  echo $result_item["item_name"]; ?></a></td>
-                                <td><?php echo $result["order_no_amount"];?> ชิ้น</td>
-                                <td><?php echo $result["order_no_amount"]*$result_item["item_price"];?> บาท</td>
-                                <?php  $total=$total+$result["order_no_amount"]*$result_item["item_price"];?>
-                            <?php
-                                $count++;
-                            }
-                            ?>
-                                    <!-- //*! datepicker */ -->
+                                <td><a href="item_detail.php?id_item=<?php echo $result_item['id_item']; ?>"><img src="image/item/<?php echo $result_item["imagelocation"] ?>" alt="..." class="   " height="50px"> <?php echo $result_item["item_name"]; ?></a></td>
+                                <td><?php echo $result["order_no_amount"]; ?> ชิ้น</td>
+                                <td><?php echo $result["order_no_amount"] * $result_item["item_price"]; ?> บาท</td>
+                                <!-- <?php $total = $total + $result["order_no_amount"] * $result_item["item_price"]; ?> -->
+                            </tr>
+                        <?php
+                            $count++;
+                        }
+                        ?>
+                        <!-- //*! datepicker */ -->
 
-                        </tr>
                     </tbody>
-                    
+
                 </table>
 
 
-                <h3 style="text-align:right">ราคารวม <?php echo $total ?> บาท.</h3>
-                
+                <h3 style="text-align:right">ราคารวม <?php
+                                                        $call_price = $conn->prepare("SELECT pay_price FROM `payment` WHERE pay_id ='$pay_id' ");
+                                                        $call_price->execute();
+                                                        $result_price = $call_price->fetch();
+                                                        echo $result_price["pay_price"]; ?> บาท.</h3>
+
+                <form action="paymnet_update_SQL.php?pay_id=<?php echo $pay_id; ?>" method="POST" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="">วันที่ชำระเงิน* : </label>
+                        <input type="date" name="DATE" required>
+                    </div>
+                    <div class="form-group">
+                        <label for=""> เวลาโดยประมาณ* : </label>
+                        <input type="time" id="TIME" name="TIME"  required>
+                    </div>
+                     
+                        <label for=""> หลักฐานการโอน* : </label>
+                        
+                        Select image to upload:
+                        <input type="file" name="fileToUpload" id="fileToUpload" required>
+                     
+                    <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+                </form>
+
             </div>
         </div>
     </div>
