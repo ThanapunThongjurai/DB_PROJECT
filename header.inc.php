56a8886a390671b3b5634944b7bea74ba012d16e
NamelessKingMeow <ubolchai2000@gmail.com>
2020-10-23 23:56:19 +0700

    Create header.inc.php

---
+++ header.inc.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+	<style type="text/css">
+		.blink {
+			text-align: center;
+			font-family: 'Prompt', sans-serif;
+			-webkit-animation: blink 1s infinite;  /* Safari 4+ */
+			-moz-animation: blink 1s infinite;  /* Fx 5+ */
+			-o-animation: blink 1s infinite;  /* Opera 12+ */
+			animation: blink 1s infinite;  /* IE 10+, Fx 29+ */
+		}
+		@-webkit-keyframes blink {
+			0%, 49% {
+				color: white;
+				background-color: red;
+			}
+			50%, 100% {
+				color: red;
+				background-color: white;
+			}
+		}
+	</style>
+	<title>Warning !!</title>
+</head>
+<body>
+	<div class="blink">
+		ห้ามระบุข้อมูลส่วนตัว ชื่อผู้ใช้ รหัสผ่าน หรือข้อมูลทางด้านการเงิน
+	</div>
+</body>
+</html>
