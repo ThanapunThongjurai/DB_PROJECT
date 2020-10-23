ef88f3ab83da699c1e69e5016f2407e07fe6c918
NamelessKingMeow <ubolchai2000@gmail.com>
2020-10-23 14:49:11 +0700

    item type

--- admin.php
+++ admin.php
@@ -7,6 +7,14 @@ if ($_SESSION["user_status"] == 0) //0 is normal
 {
   header("Location: login.php");
 }
+$item_type = $conn->prepare("SELECT * FROM `item_type`");
+$item_type->execute();
+$item_type_count = 0;
+while ($item_type_result = $item_type->fetch()) {
+   $item_type_array[$item_type_count][0] = $item_type_result["item_type_id"];
+   $item_type_array[$item_type_count][1] = $item_type_result["item_type_name"];
+   $item_type_count++ . "<br>";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -41,7 +49,7 @@ if ($_SESSION["user_status"] == 0) //0 is normal
       ?>
       <div class="">
         <table class="table">
-        <a type="button" class="btn btn-outline-success btn-lg" href="item_insert.php">item_insert</a>
+          <a type="button" class="btn btn-outline-success btn-lg" href="item_insert.php">item_insert</a>
           <thead class="thead-dark">
             <tr>
               <th scope="col">item_id</th>
@@ -54,15 +62,31 @@ if ($_SESSION["user_status"] == 0) //0 is normal
           </thead>
           <?php
           while ($result = $stmt->fetch()) {
+
           ?>
+
+
             <tbody>
               <tr>
                 <th scope="row"><?php echo $result["id_item"]; ?></th>
                 <td><?php echo $result["item_name"]; ?></td>
                 <td><?php echo $result["item_price"]; ?></td>
-                <td><?php echo $result["item_type"]; ?></td>
+                <td>
+                  <?php
+
+                  for($i = 0 ; $i <$item_type_count;$i++)
+                  {
+                    if($result["item_type"] == $item_type_array[$i][0])
+                    {
+                      echo $result["item_type"].$item_type_array[$i][1];
+                    }
+                  }
+                  // echo $result["item_type"];
+
+                  ?>
+                </td>
                 <td><?php echo $result["item_amount"]; ?></td>
-                <td><img src="image/item/<?php echo $result["imagelocation"] ; ?>" height="40px"></td>
+                <td><img src="image/item/<?php echo $result["imagelocation"]; ?>" height="40px"></td>
                 <td><a type="button" class="btn btn-outline-secondary" href="item_update.php?id_item=<?php echo $result["id_item"]; ?>">UPDATE</a></td>
                 <td><a type="button" class="btn btn-outline-danger" href="item_delete.php?id_item=<?php echo $result["id_item"]; ?>">DELETE</a></td>
               </tr>
--- item_detail.php
+++ item_detail.php
@@ -5,6 +5,15 @@ require_once('connect.php');
 $id_item = $_GET['id_item'];
 $stmt = $conn->prepare("SELECT * FROM `item` WHERE id_item = $id_item");
 $stmt->execute();
+
+$item_type = $conn->prepare("SELECT * FROM `item_type`");
+$item_type->execute();
+$item_type_count = 0;
+while ($item_type_result = $item_type->fetch()) {
+   $item_type_array[$item_type_count][0] = $item_type_result["item_type_id"];
+   $item_type_array[$item_type_count][1] = $item_type_result["item_type_name"];
+   $item_type_count++ . "<br>";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,13 +39,14 @@ $stmt->execute();
 <body>
 
 
-    <?php include_once (__DIR__) . ('/include/navbar.php'); $id_item = $stmt->fetch()?>
+    <?php include_once (__DIR__) . ('/include/navbar.php');
+    $id_item = $stmt->fetch() ?>
     <div class="container card mt-5">
         <div class="row">
             <div class="col-sm-4">
                 <img src="image/item/<?php echo $id_item["imagelocation"] ?>" class="card-img-top" alt="...">
                 <button type="button" class="btn btn-primary">
-                
+
                     <a class="btn btn-primary" href="cart.php?id_item=<?php echo $id_item['id_item']; ?>&act=add" role="button">
                         <h1>หยิบลงตะกร้า</h1>
                     </a>
@@ -46,12 +56,24 @@ $stmt->execute();
             <div class="col-sm-8 mt-5">
                 <p class="">ชื่อสินค้า : <?php echo $id_item["item_name"] ?></p>
                 <p class="">ราคา : <?php echo $id_item["item_price"] ?></p>
+                <p> ประเภท : 
+                    <?php
+
+                    for ($i = 0; $i < $item_type_count; $i++) {
+                        if ($id_item["item_type"] == $item_type_array[$i][0]) {
+                            echo /*$id_item["item_type"] . */$item_type_array[$i][1];
+                        }
+                    }
+                    // echo $result["item_type"];
+
+                    ?>
+                </p>
                 <p class="">คลัง : <?php echo $id_item["item_amount"] ?></p>
                 <p class="">คำอธิบาย : <?php echo $id_item["item_disc"] ?></p>
             </div>
         </div>
 
-        </div>
+    </div>
     </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
