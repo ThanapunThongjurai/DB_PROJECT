bb6ffa9788e6cb3058653eb26b017bf79e11347e
multifriend <55123111+multifriend@users.noreply.github.com>
2020-10-25 02:01:12 +0700

    fixed navbar2

--- include/navbar2.php
+++ include/navbar2.php
@@ -19,11 +19,11 @@ if (!isset($_SESSION)) {
           <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
         </li>
       </ul>
-    </div>
-    <form class="form-inline">
+      <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">ค้นหา</button>
     </form>
+    </div>
   </div>
 
   <?php
